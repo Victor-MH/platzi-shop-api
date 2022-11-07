ffaa83297e364acfbc00543a7a09dc31502e020b
Victor Muñoz <victormu497@gmail.com>
2022-11-06 18:23:17 -0600

    7 Pool de conexiones

--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,21 @@
 const boom = require('@hapi/boom');
 
-const getConnection = require('../libs/postgres')
+const pool = require('../libs/postgres.pool')
 
 
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => console.log(err));
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const rta = await client.query('SELECT * FROM tasks');
+    const query = 'SELECT * FROM tasks'
+    const rta = await this.pool.query(query);
     return rta.rows;
   }
 
