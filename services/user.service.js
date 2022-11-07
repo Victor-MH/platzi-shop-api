afbc4dc8e9ac2cc3d621fbef9289bd76e0971e83
Victor Muñoz <victormu497@gmail.com>
2022-11-06 18:04:16 -0600

    6 Conectando node y postgresql

--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,8 @@
 const boom = require('@hapi/boom');
 
+const getConnection = require('../libs/postgres')
+
+
 class UserService {
   constructor() {}
 
@@ -8,7 +11,9 @@ class UserService {
   }
 
   async find() {
-    return [];
+    const client = await getConnection();
+    const rta = await client.query('SELECT * FROM tasks');
+    return rta.rows;
   }
 
   async findOne(id) {
